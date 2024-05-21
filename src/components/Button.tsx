5894d73fe3acf9a878296f1c2934ef86d547f0bb
AV-MMI <villalobos.aaron@hotmail.com>
2024-05-21 07:55:53 -0400

    closes #10

---
+++ src/components/Button.tsx
@@ -0,0 +1,18 @@
+export { Button };
+
+interface ButtonProps {
+    text: string;
+    className: string;
+    onClick: MouseEvent;
+}
+
+function Button({text, className, onClick} : ButtonProps){
+    const defaultClassNames: string = "btn"; 
+
+    return (
+        <button className={`${defaultClassNames} ${className}`} onClick={(e) => {
+            e.stopPropagation();
+            onClick(e);
+        }}>{text}</button>
+    )
+}
