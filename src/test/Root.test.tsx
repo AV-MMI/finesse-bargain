4813b697e068578823c7bc03217f08225465b943
AV-MMI <villalobos.aaron@hotmail.com>
2024-05-22 16:51:17 -0400

    closes #15

--- src/test/Root.test.tsx
+++ src/test/Root.test.tsx
@@ -2,18 +2,25 @@ import { render, screen } from "@testing-library/react";
 import { expect, afterEach, describe } from 'vitest';
 import { Root } from "../components/Root.tsx";
 import { describe, it, expect } from 'vitest';
+import { BrowserRouter } from "react-router-dom";
 
 describe('Root component', () => {
   it('render correct heading', () => {
-    render(<div><Root /></div>);
-    expect(true).toBe(true)
+    render(<Root />, {wrapper: BrowserRouter} );
+    expect(screen.getByRole("heading").textContent).toMatch(/Finesse Bargain/i)
   })
+
   it('render menu', () => {
-    expect(true).toBe(true);
+    render(<Root />, {wrapper: BrowserRouter});
+
+    const navigationLinks: HTMLElement[] = screen.getAllByText(/(Home|Shop|About)/i);
+    expect(navigationLinks.length).toBe(3);
   });
 
   it('render theme button', () => {
-    expect(false).toBe(false);
+    render(<Root />, {wrapper: BrowserRouter});
+
+    expect( screen.getByText(/(Light Theme|Dark Theme)/i).textContent ).toMatch(/(Dark theme|Light theme)/i);
   });
 
   it('render Cart modal', () => {
